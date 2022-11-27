08b50ba6b9c22f47955ef4e41722d3cbf45338cc
visitorckw <2001wayne@gmail.com>
2022-11-27 16:52:41 +0800

    feat: add advanceUcbAgent by combining advanceGreedy and ucb

--- loader.py
+++ loader.py
@@ -5,6 +5,7 @@ import advanceGreedyAgent
 import ucbAgent
 import epsilonDeltaAgent
 import mlAgent
+import advanceUcb
 import sklearn
 from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet
 from sklearn.neighbors import KNeighborsRegressor
@@ -14,4 +15,4 @@ from sklearn.svm import SVR
 if __name__ == '__main__':
     G = game.game(50, 1000)
     # G.run(mlAgent.agent(50, 1000, LinearRegression(), True, 100),mlAgent.agent(50, 1000, KNeighborsRegressor(), True, 100), False)
-    G.run(greedyAgent.agent(), advanceGreedyAgent.agent(), True)
+    G.run(advanceUcb.agent(), greedyAgent.agent(), True)
