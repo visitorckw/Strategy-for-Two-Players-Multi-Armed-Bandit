e4a1591e34c0c7cb42326abda1b1abb1d666e312
fourcolor <james1qaz2wsx12qw@gmail.com>
2022-11-30 22:12:44 +0800

    parallel

--- advanceUcbParallel.py
+++ advanceUcbParallel.py
@@ -1,9 +1,13 @@
 import math
+import threading
 
 class agent():
-    def __init__(self, c = math.sqrt(2)):
+    def __init__(self, c=math.sqrt(2)):
         self.c = c
         self.delta = 1e-9
+        self.p = 0
+        self.lock = threading.Lock() 
+
     def likely(self, prob, agent, machine, my_history_choice, opp_history_choice, my_history_reward):
         p = prob
         ans = 1.0
@@ -36,21 +40,40 @@ class agent():
         if ismul :
             return ans
         return 0
+
+    def worker(self, jobs):
+        localMax = 0
+        for i in jobs:
+            tmp = self.likely(*i)
+            if tmp > localMax:
+                localMax = tmp
+        self.lock.acquire()
+        if self.p < localMax:
+            self.p = localMax
+        self.lock.release()
+
+            
     def dfdx(self, x, agent, machine, my_history_choice, opp_history_choice, my_history_reward):
-        y = self.likely(x, agent, machine, my_history_choice, opp_history_choice, my_history_reward)
-        y1 = self.likely(x+self.delta, agent, machine, my_history_choice, opp_history_choice, my_history_reward)
+        y = self.likely(x, agent, machine, my_history_choice,
+                        opp_history_choice, my_history_reward)
+        y1 = self.likely(x+self.delta, agent, machine,
+                         my_history_choice, opp_history_choice, my_history_reward)
         return (y1 - y) / self.delta
+
     def binary_search(self, agent, machine, my_history_choice, opp_history_choice, my_history_reward):
         delta = 1e-9
         L = 0
         R = 1 - delta
         while L < R:
-            dL = self.dfdx(L, agent, machine, my_history_choice, opp_history_choice, my_history_reward)
-            dR = self.dfdx(R, agent, machine, my_history_choice, opp_history_choice, my_history_reward)
+            dL = self.dfdx(L, agent, machine, my_history_choice,
+                           opp_history_choice, my_history_reward)
+            dR = self.dfdx(R, agent, machine, my_history_choice,
+                           opp_history_choice, my_history_reward)
             if dL * dR >= 0:
                 break
             M = (L + R) / 2
-            dM = self.dfdx(M, agent, machine, my_history_choice, opp_history_choice, my_history_reward)
+            dM = self.dfdx(M, agent, machine, my_history_choice,
+                           opp_history_choice, my_history_reward)
             if abs(dM) <= self.delta:
                 L = R = M
                 break
@@ -59,19 +82,21 @@ class agent():
             else:
                 R = M - self.delta
         return (L + R) / 2
+
     def linear_search(self, agent, machine, my_history_choice, opp_history_choice, my_history_reward):
         N = 100
-        maxLikely = -1
-        ans = -1
-        for i in range(N+1):
-            p = i / N
-            like = self.likely(p, agent, machine, my_history_choice, opp_history_choice, my_history_reward)
-            if like > maxLikely:
-                maxLikely = like
-                ans = p
-        return ans
+        self.p = 0
+        threads = []
+        list = [(i/N, agent, machine, my_history_choice,opp_history_choice, my_history_reward) for i in range(N+1)]
+        for i in range(4):
+            threads.append(threading.Thread(target=self.worker,args=(list[int((N+1)/4*i):int((N+1)/4*(i+1))],)))
+            threads[i].start()
+        for i in range(4):
+            threads[i].join()
+        return self.p
+
     def play(self, agent, machine_numbers, total_round, current_round, my_total_rewards, my_history_choice, opp_history_choice, my_history_reward, my_push_distribute, opp_push_distribute, my_reward_distribute):
-        if len(my_history_choice) < machine_numbers: # 尚未嘗試過所有機器
+        if len(my_history_choice) < machine_numbers:  # 尚未嘗試過所有機器
             for i in range(machine_numbers):
                 if i not in my_history_choice:
                     return i
@@ -84,7 +109,8 @@ class agent():
         maxUCB = -1
         choice = -1
         for i in range(machine_numbers):
-            ev = self.linear_search(agent, i, my_history_choice, opp_history_choice, my_history_reward)
+            ev = self.linear_search(
+                agent, i, my_history_choice, opp_history_choice, my_history_reward)
             ucb = ev + self.c * math.sqrt(math.log(N) / n[i])
             if ucb > maxUCB:
                 maxUCB = ucb
--- loader.py
+++ loader.py
@@ -4,6 +4,7 @@ import greedyAgent
 import advanceGreedyAgent
 import ucbAgent
 import epsilonDeltaAgent
+import advanceUcbParallel
 import mlAgent
 import advanceUcb
 import sklearn
@@ -13,6 +14,6 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.svm import SVR
 
 if __name__ == '__main__':
-    G = game.game(50, 1000)
+    G = game.game(50, 500)
     # G.run(mlAgent.agent(50, 1000, LinearRegression(), True, 100),mlAgent.agent(50, 1000, KNeighborsRegressor(), True, 100), False)
-    G.run(advanceGreedyAgent.agent(), greedyAgent.agent(), True)
+    G.run(advanceUcb.agent(), greedyAgent.agent(), False)
